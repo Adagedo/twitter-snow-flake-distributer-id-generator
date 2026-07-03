1b9cee9b46abe80ea605082a2dc6996886300010
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-07-03 13:13:18 +0100

    added redis for key value storage

---
+++ distributed-id-generator-twitter-snow-flake/src/main/java/code/adagedo/distributed_id_generator_twitter_snow_flake/config/RedisConfiguration.java
@@ -0,0 +1,35 @@
+package code.adagedo.distributed_id_generator_twitter_snow_flake.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        // String serializers
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        // Long serializers
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+        redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Long.class));
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+}
---
+++ distributed-id-generator-twitter-snow-flake/src/main/java/code/adagedo/distributed_id_generator_twitter_snow_flake/service/RedisKeyValueService.java
@@ -0,0 +1,41 @@
+package code.adagedo.distributed_id_generator_twitter_snow_flake.service;
+
+import code.adagedo.distributed_id_generator_twitter_snow_flake.models.Datacenter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisKeyValueService {
+
+    private final RedisTemplate<String, Long> redisTemplate;
+
+
+    public void save(){
+        String KEY = "snowflake:datacenter";
+        if (redisTemplate.hasKey(KEY)){
+            HashOperations<String, String, Long> hash = redisTemplate.opsForHash();
+
+            hash.put(KEY, Datacenter.EU_CENTRAL_1.getName(), Datacenter.EU_CENTRAL_1.getId());
+            hash.put(KEY, Datacenter.US_WEST_2.getName(), Datacenter.US_WEST_2.getId());
+            hash.put(KEY, Datacenter.US_EAST_1.getName(), Datacenter.US_EAST_1.getId());
+
+            hash.put(KEY, Datacenter.EUROPE_WEST1.getName(), Datacenter.EUROPE_WEST1.getId());
+            hash.put(KEY, Datacenter.EUROPE_WEST2.getName(), Datacenter.EUROPE_WEST2.getId());
+            hash.put(KEY, Datacenter.ASIA_SOUTHEAST1.getName(), Datacenter.ASIA_SOUTHEAST1.getId());
+
+            hash.put(KEY, Datacenter.NORTH_EUROPE.getName(), Datacenter.NORTH_EUROPE.getId());
+            hash.put(KEY, Datacenter.SOUTHEAST_ASIA.getName(), Datacenter.SOUTHEAST_ASIA.getId());
+            hash.put(KEY, Datacenter.JAPAN_EAST.getName(), Datacenter.JAPAN_EAST.getId());
+        }
+    }
+
+    private long getDataCenterId(String key, String dataCenterName){
+        HashOperations<String, String, Long> hash = redisTemplate.opsForHash();
+        return hash.get(key, dataCenterName);
+    }
+}
